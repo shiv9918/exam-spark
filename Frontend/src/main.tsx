ab0ad30da7c5c1c9d93bd8286b1d8024106959c6
shiv9918 <shivammishra01329@gmail.com>
2025-06-22 00:42:43 +0530

    debug: Add console log to check VITE_API_URL on Vercel

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './contexts/ThemeContext.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log("Checking Environment Variables...");
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);

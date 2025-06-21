3ee76028686d7942709d30434ccabdadcd3c5020
shiv9918 <shivammishra01329@gmail.com>
2025-06-22 00:46:16 +0530

    refactor: Remove debugging console logs from main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,13 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './contexts/ThemeContext.tsx'
 
-console.log("Checking Environment Variables...");
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
